2e1d8f55c348efb7db8d3fd20f31acd2875d8772
LanSico <291909157@qq.com>
2017-04-03 23:49:18 +0800

    签到。未完成版

---
+++ exp024.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int comp(int a[], int b[], int c, int d,int e) {
+	int i, j, k;
+	i = c;
+
+}
+
+main() {
+	int s[];
+	printf("请输入要排序的十个数：");
+	scanf("%d %d %d %d %d %d %d %d %d %d", &s[0], &s[1], &s[2], &s[3], &s[4], &s[5], &s[6], &s[7], &s[8], &s[9]);
+
+}
