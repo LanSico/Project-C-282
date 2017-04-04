b73ced43e7797b2d3bbb43a6793278a84fbf014a
LanSico <291909157@qq.com>
2017-04-04 22:30:46 +0800

    签到非完成版

--- exp024.c
+++ exp024.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<conio.h>
 
-int ch(int m, int n)
+void ch(int m, int n)
 {
 	int temp;
 	temp = m;
@@ -13,14 +13,36 @@ int comp(int a[], int b[], int c, int d,int e) {
 	int i, j, k;
 	i = c;
 	j = d + 1;
-	if(a[i]>a[j])
-
-
+	for (k = c;k <= e;k++)
+	{
+	if (i == d)
+	{
+		b[k] = a[j];
+		j++;
+	}
+	else if (j == e)
+	{
+		b[k] = a[i];
+		i++;
+	}
+	else if (a[i] > a[j])
+	{
+		b[k] = a[j];
+		j++;
+	}
+	else
+	{
+		b[k] = a[i];
+		i++;
+	}
+	}
 }
 
 main() {
-	int s[10];
+	int s[10],r[10];
+	int u, v, w,x;
 	printf("请输入要排序的十个数：");
 	scanf("%d %d %d %d %d %d %d %d %d %d", &s[0], &s[1], &s[2], &s[3], &s[4], &s[5], &s[6], &s[7], &s[8], &s[9]);
-
+	for (x)
+		comp(s, r, u, v, w);
 }
